5e0d91ce756afaa1e5d6de2ea575ac67fe091db3
vwxwv1 <61165141+vwxwv1@users.noreply.github.com>
2021-10-13 19:36:46 -0400

    Update Removefiles.py

--- Removefiles.py
+++ Removefiles.py
@@ -2,28 +2,62 @@ import os
 import shutil
 import time
 
-path = input("The path of the folder.")
-days = input("Desired days old of the files you want to delete.")
-ctime = str(os.stat(path).st_ctime)
+def main():
+	deleted_folders_count = 0
+	deleted_files_count = 0
+    
+	path = "/path_deleted_soon"
+	days = 30
+	seconds = time.time() - (days * 24 * 60 * 60)
 
-os.path.exists(path)
-os.walk(path)
-os.path.join(path)
-os.stat(path).st_ctime
+	if os.path.exists(path):
+		for root_folder, folders, files in os.walk(path):
+			if seconds >= get_file_or_folder_age(root_folder):
+				remove_folder(root_folder)
+				deleted_folders_count += 1 
+				break
+			else:
+				for folder in folders:
+					folder_path = os.path.join(root_folder, folder)
+					if seconds >= get_file_or_folder_age(folder_path):
+						remove_folder(folder_path)
+						deleted_folders_count += 1
+				for file in files:
+					file_path = os.path.join(root_folder, file)
+					if seconds >= get_file_or_folder_age(file_path):
+						remove_file(file_path)
+						deleted_files_count += 1 
+		else:
+			if seconds >= get_file_or_folder_age(path):
+				remove_file(path)
+				deleted_files_count += 1 
+	else:
+		print(f'"{path}" is not found')
+		deleted_files_count += 1
 
-if (ctime < days): 
-    list_of_files = os.listdir(path)
+	print(f"Total folders deleted: {deleted_folders_count}")
+	print(f"Total files deleted: {deleted_files_count}")
+    
 
-    for file in list_of_files:
-        name, ext = os.path.splitext(file)
+def remove_folder(path):
+	if not shutil.rmtree(path):
+		print(f"{path} is removed successfully")
+	else:
+		print(f"Unable to delete the "+path)
 
-        ext = ext[1:]
+        
+def remove_file(path):
+	if not os.remove(path):
+		print(f"{path} is removed successfully")
+	else:
+		print("Unable to delete the "+path)
 
-        if ext == '':
-            continue
 
-        if os.path.exists(path+'/'+ext):
-            os.remove(path)
+def get_file_or_folder_age(path):
+	ctime = os.stat(path).st_ctime
+    
+	return ctime
 
-        else:
-            shutil.rmtree(path)
+
+if __name__ == '__main__':
+	main()
